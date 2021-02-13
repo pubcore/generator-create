000237f6b0e2e8ec3cd0ec14cd5023d96ff100ea
jacklrs <github@rushsoft.de>
2021-02-13 23:11:01 +0100

    feat: add prettier to created generator

--- app/index.js
+++ app/index.js
@@ -1,54 +1,89 @@
 const Generator = require('yeoman-generator'),
-	{basename, resolve} = require('path'),
-	{readdirSync} = require('fs')
+	{ basename, resolve } = require('path'),
+	{ readdirSync } = require('fs')
 
 module.exports = class extends Generator {
 	constructor(args, opts) {
 		super(args, opts)
 	}
-	initializing(){
+	initializing() {
 		var localName = this.appname.replace(/\s+/g, '-')
-		if(readdirSync(this.destinationRoot()).length){
-			this.log.error('working directory is not empty, beware hidden files (dot-files)')
+		if (readdirSync(this.destinationRoot()).length) {
+			this.log.error(
+				'working directory is not empty, beware hidden files (dot-files)'
+			)
 			process.exit(1)
 		}
-		if(!localName.match(/^generator-/)){
+		if (!localName.match(/^generator-/)) {
 			this.log.error('working directory name must start with "generator-"')
 		}
 		this.initial = {
 			scope: basename(resolve(process.cwd(), '..')),
 			localName,
-			generatorName: localName.replace(/generator-/, '')
+			generatorName: localName.replace(/generator-/, ''),
 		}
 	}
 	async prompting() {
-		var {scope, localName} = this.initial
-		this.answers = await this.prompt([{
-			type: 'input', name: 'name', default : () => `@${scope}/${localName}`,
-			message : 'Your project name',
-		},{
-			type: 'input', name: 'description',
-			message: 'Package description',
-		},{
-			type: 'input', name: 'license', default: 'MIT',
-			message: 'License',
-		},{
-			type: 'input', name: 'author', default: () => scope,
-			message: 'Author'
-		},{
-			type: 'input', name: 'repository',
-			message: 'Repository uri',
-		}])
+		var { scope, localName } = this.initial
+		this.answers = await this.prompt([
+			{
+				type: 'input',
+				name: 'name',
+				default: () => `@${scope}/${localName}`,
+				message: 'Your project name',
+			},
+			{
+				type: 'input',
+				name: 'description',
+				message: 'Package description',
+			},
+			{
+				type: 'input',
+				name: 'license',
+				default: 'MIT',
+				message: 'License',
+			},
+			{
+				type: 'input',
+				name: 'author',
+				default: () => scope,
+				message: 'Author',
+			},
+			{
+				type: 'input',
+				name: 'repository',
+				message: 'Repository uri',
+			},
+		])
 	}
-	writing(){
-		var replacements = {...this.answers, ...this.initial}
-		this.fs.copy(this.templatePath('static/**/*'), this.destinationPath('.'), {globOptions:{dot:true}} )
-		this.fs.copyTpl(this.templatePath('package-json'), this.destinationPath('./package.json'), replacements)
-		this.fs.copyTpl(this.templatePath('README.md'), this.destinationPath('./README.md'), replacements)
+	writing() {
+		var replacements = { ...this.answers, ...this.initial }
+		this.fs.copy(this.templatePath('static/**/*'), this.destinationPath('.'), {
+			globOptions: { dot: true },
+		})
+		this.fs.copyTpl(
+			this.templatePath('package-json'),
+			this.destinationPath('./package.json'),
+			replacements
+		)
+		this.fs.copyTpl(
+			this.templatePath('README.md'),
+			this.destinationPath('./README.md'),
+			replacements
+		)
 	}
-	install(){
+	install() {
 		this.log('Install packages ...')
-		this.spawnCommandSync('npm', ['i', '--save-dev', 'eslint', 'mocha', 'yeoman-test'])
+		this.spawnCommandSync('npm', [
+			'i',
+			'--save-dev',
+			'eslint',
+			'mocha',
+			'yeoman-test',
+			'yeoman-assert',
+			'husky',
+			'prettier',
+		])
 		this.spawnCommandSync('npm', ['i', 'yeoman-generator'])
 		this.spawnCommandSync('npx', ['eslint', '--init'])
 	}
--- app/templates/static/app/index.js
+++ app/templates/static/app/index.js
@@ -1,52 +1,86 @@
 const Generator = require('yeoman-generator'),
-	{basename, resolve} = require('path'),
-	{readdirSync} = require('fs')
+	{ basename, resolve } = require('path'),
+	{ readdirSync } = require('fs')
 
 module.exports = class extends Generator {
 	constructor(args, opts) {
 		super(args, opts)
 	}
-	initializing(){
+	initializing() {
 		var localName = this.appname.replace(/\s+/g, '-')
-		if(readdirSync(this.destinationRoot()).length){
-			this.log.error('working directory is not empty, beware hidden files (dot-files)')
+		if (readdirSync(this.destinationRoot()).length) {
+			this.log.error(
+				'working directory is not empty, beware hidden files (dot-files)'
+			)
 			process.exit(1)
 		}
 		this.initial = {
 			scope: basename(resolve(process.cwd(), '..')),
 			localName,
-			generatorName: localName.replace(/generator-/, '')
+			generatorName: localName.replace(/generator-/, ''),
 		}
 	}
 	async prompting() {
-		var {scope, localName} = this.initial
-		this.answers = await this.prompt([{
-			type: 'input', name: 'name', default : () => `@${scope}/${localName}`,
-			message : 'Your project name',
-		},{
-			type: 'input', name: 'description',
-			message: 'Package description',
-		},{
-			type: 'input', name: 'license', default: 'MIT',
-			message: 'License',
-		},{
-			type: 'input', name: 'author', default: () => scope,
-			message: 'Author'
-		},{
-			type: 'input', name: 'repository',
-			message: 'Repository uri',
-		}])
+		var { scope, localName } = this.initial
+		this.answers = await this.prompt([
+			{
+				type: 'input',
+				name: 'name',
+				default: () => `@${scope}/${localName}`,
+				message: 'Your project name',
+			},
+			{
+				type: 'input',
+				name: 'description',
+				message: 'Package description',
+			},
+			{
+				type: 'input',
+				name: 'license',
+				default: 'MIT',
+				message: 'License',
+			},
+			{
+				type: 'input',
+				name: 'author',
+				default: () => scope,
+				message: 'Author',
+			},
+			{
+				type: 'input',
+				name: 'repository',
+				message: 'Repository uri',
+			},
+		])
 	}
-	writing(){
-		var replacements = {...this.answers, ...this.initial}
+	writing() {
+		var replacements = { ...this.answers, ...this.initial }
 
-		this.fs.copy(this.templatePath('static/**/*'), this.destinationPath('.'), {globOptions:{dot:true}} )
-		this.fs.copyTpl(this.templatePath('package-json'), this.destinationPath('./package.json'), replacements)
-		this.fs.copyTpl(this.templatePath('README.md'), this.destinationPath('./README.md'), replacements)
+		this.fs.copy(this.templatePath('static/**/*'), this.destinationPath('.'), {
+			globOptions: { dot: true },
+		})
+		this.fs.copyTpl(
+			this.templatePath('package-json'),
+			this.destinationPath('./package.json'),
+			replacements
+		)
+		this.fs.copyTpl(
+			this.templatePath('README.md'),
+			this.destinationPath('./README.md'),
+			replacements
+		)
 	}
-	install(){
+	install() {
 		this.log('Install packages ...')
-		this.spawnCommandSync('npm', ['i', '--save-dev', 'eslint', 'mocha', 'yeoman-test'])
+		this.spawnCommandSync('npm', [
+			'i',
+			'--save-dev',
+			'eslint',
+			'mocha',
+			'yeoman-test',
+			'husky',
+			'prettier',
+		])
 		this.spawnCommandSync('npm', ['i', 'yeoman-generator'])
 		this.spawnCommandSync('npx', ['eslint', '--init'])
 	}
